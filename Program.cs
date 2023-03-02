fa112aa3c3fae168d27c35319ca8f1c13a383a0c
Xatiam <xatiam116@gmail.com>
2023-03-03 00:24:58 +0300

    Задача1

--- Program.cs
+++ Program.cs
@@ -43,5 +43,18 @@
 
 // Console.WriteLine("Наибольшее из введённых чисел -> " + max);
 
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
 
-
+Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+  Console.WriteLine("Number " + number + " is even");
+}
+else
+{
+  Console.WriteLine("Number " + number + " is odd");
+}
