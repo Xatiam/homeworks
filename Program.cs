16130083e28ade9715484fe961e9f7e9affa6025
Xatiam <xatiam116@gmail.com>
2023-03-03 00:16:36 +0300

    Задача1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Домашние задание.
+// Задача2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Input a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b)
+// {
+//   Console.Write("max = " + a);
+//   Console.WriteLine("min = " + b);
+// }
+// else
+// {
+//   Console.Write("max = " + b);
+//   Console.WriteLine("min = " + a);
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите 3 числа:");
+// int num_1 = Convert.ToInt32(Console.ReadLine());
+// int num_2 = Convert.ToInt32(Console.ReadLine());
+// int num_3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = num_1;
+
+// if (num_2 > max)
+// {
+//   max = num_2;
+// }
+
+// if (num_3 > max)
+// {
+//   max = num_3;
+// }
+
+// Console.WriteLine("Наибольшее из введённых чисел -> " + max);
+
+
+
